324e57032e3bb942a50f56d8fc1d018b45c8c835
Matthew Egan <matthewj.egan@hotmail.com>
2018-04-26 11:27:08 +1000

    use pytest style tests

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+import pytest
 
 from main import fibonacci
 
@@ -7,7 +8,8 @@ class FibonacciTests(TestCase):
         correct_sequence = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
         for index in range(len(correct_sequence)):
             response = fibonacci(index)
-            self.assertEqual(response, correct_sequence[index])
+            assert response == correct_sequence[index]
 
     def test_raise_value_error_on_negative_input(self):
-        self.assertRaises(ValueError, fibonacci, -1)
+        with pytest.raises(ValueError):
+            fibonacci(-1)
